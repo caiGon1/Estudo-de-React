42b82475a65e5e593c2e0760a7907118d28182bb
Caio Silva <caio.goncalvesananias@gmail.com>
2025-12-23 13:31:54 -0300

    Correction tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,15 +1,15 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
 
-function Tasks(props) {
+function Tasks({tasks, onTaskClick, onTaskDelete}) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md  shadow">
-      {props.tasks.map(
+      {tasks.map(
         (
           tasks //para cada tarefa
         ) => (
           <li key={tasks.id} className="flex gap-2">
             <button
-              onClick={() => props.onTaskClick(tasks.id)}
+              onClick={() => onTaskClick(tasks.id)}
               className={`bg-slate-400 text-left text-white p-2 rounded-md w-full pointer-events-auto cursor-pointer ${
                 tasks.isCompleted && "line-through"
               }`}
@@ -20,7 +20,7 @@ function Tasks(props) {
               <ChevronRightIcon />
             </button>
             <button className="bg-slate-400 p-2 rounded-md text-white pointer-events-auto cursor-pointer"
-            onClick={()=>props.onTaskDelete(tasks.id)}>
+            onClick={()=>onTaskDelete(tasks.id)}>
               <TrashIcon/>
             </button>
           </li> //renderize um paragrafo
