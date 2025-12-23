91f3c29a08ab40b0ecc3d7cf6541882b99cf6237
Caio Silva <caio.goncalvesananias@gmail.com>
2025-12-23 11:35:28 -0300

    Mudança de borda

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
   //State
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-500px">
+      <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
