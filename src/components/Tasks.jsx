e85f4fb712f74aadefbfe3118555220a64e5b3de
Caio Silva <caio.goncalvesananias@gmail.com>
2025-12-24 09:37:38 -0300

    Detalhes da Pagina

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,16 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-function Tasks({tasks, onTaskClick, onTaskDelete}) {
+function Tasks({ tasks, onTaskClick, onTaskDelete }) {
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(tasks) {
+    const query = new URLSearchParams()
+    query.set("title", tasks.title)
+    query.set("description", tasks.description)
+    navigate(`/task?${query.toString()}`);
+  }
+
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md  shadow">
       {tasks.map(
@@ -16,12 +26,17 @@ function Tasks({tasks, onTaskClick, onTaskDelete}) {
             >
               {tasks.title}
             </button>
-            <button className="bg-slate-400 p-2 rounded-md text-white pointer-events-auto cursor-pointer">
+            <button
+              onClick={() => onSeeDetailsClick(tasks)}
+              className="bg-slate-400 p-2 rounded-md text-white pointer-events-auto cursor-pointer"
+            >
               <ChevronRightIcon />
             </button>
-            <button className="bg-slate-400 p-2 rounded-md text-white pointer-events-auto cursor-pointer"
-            onClick={()=>onTaskDelete(tasks.id)}>
-              <TrashIcon/>
+            <button
+              className="bg-slate-400 p-2 rounded-md text-white pointer-events-auto cursor-pointer"
+              onClick={() => onTaskDelete(tasks.id)}
+            >
+              <TrashIcon />
             </button>
           </li> //renderize um paragrafo
         )
